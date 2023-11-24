bdc8adc1880075d0bc305048c25147ff7842abcd
Vectrino314 <vectrino314@gmail.com>
2023-11-24 16:21:17 -0600

    Added Articles Listing Feature

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root 'pages#home'
-  resources :articles, only: [:show]
+  resources :articles, only: [:show, :index]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
